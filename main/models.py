64851accec1b16f9b7ac322049f31ce4e1059d16
Patryk <patrykdziedzioch@gmail.com>
2019-03-23 20:24:39 +0100

    [+] robimy tylko w tym folderze, reszty nie ruszac

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Categories(models.Model):
+    sub_title = models.CharField(max_length=200)
+    sub_content = models.TextField()
+    sub_published = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.sub_title
